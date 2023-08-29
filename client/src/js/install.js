1311fd73bec10c62146d95833690e08129fb5477
Chris L <chris@larsonwerks.com>
2023-08-29 08:39:07 -0700

    added functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,36 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    //store the triggered events
+
+    // Store the triggered events
     window.deferredPrompt = event;
-    //remove the hiddent class from the button
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
-});
+  });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+  
+  const promptEvent = window.deferredPrompt;
 
-    if (!promptEvent) {
-        return;
-    }
+  if (!promptEvent) {
+   return;
+  }
 
-    //shows prompt
-    promptEvent.prompt();
-
-    //reset the deferred prompt variable, use once
-    window.deferredPrompt = null;
-
-    butInstall.classList.toggle('hidden', true);
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    //clear prompt
-    window.deferredPrompt = null;
-})
-
-
-
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -17,55 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // TODO: Add and configure workbox plugins for a service worker and manifest file.
-      //repurposed code that was in the mini-project
+      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E.'
+        title: 'Contact Cards'
       }),
-      //adds the custom service worker
+     
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      
-      //create a manifest.json file
+
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE',
-        short_name: 'JATE',
-        description: 'Text Editor',
-        background_color: '#000',
-        theme_color: '#000',
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
         start_url: './',
         publicPath: './',
         icons: [
           {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128],
-          destination: path.join('assets', 'icons'),
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
-      })
+      }),
     ],
-    
+
     module: {
-      // TODO: Add CSS loaders and babel to webpack.
-      //repurposed code that was in the mini-project
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
         {
-          test:/\.m?js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/presetn-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/tranform-runtime'],
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
